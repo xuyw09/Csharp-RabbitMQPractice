e1badb69a4c126c441e610c7d39699439561eea6
xuyw <xuyw09@126.com>
2020-08-11 16:49:22 +0800

    添加项目文件。

---
+++ MQSend/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MQSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    string message = input;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine("[x] sent {0}", message);
+                }
+
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
